22b88cd90d472740d8d19d0c48236a1b29dc2e5e
IdaHolmen <143997448+IdaHolmen@users.noreply.github.com>
2023-11-16 16:10:06 +0100

    Trying out a method for sorting

---
+++ js/books.js
@@ -0,0 +1,36 @@
+const books = [
+	{	
+	title: 'Harry Potter and the Prisoner of Azkaban',
+	author: 'J. K. Rowling',
+	popular: true,
+	group: 'young adult',
+	fiction: true,
+	},
+
+	{ 
+	title: 'The Hitchhikers Guide to the Galaxy',
+	author: 'Douglas Adams',
+	popular: true,
+	group: 'young adult',
+	fiction: true,
+	},
+
+	{ 
+	title: '',
+	author: '',
+	popular: ,
+	group: '',
+	fiction: ,
+	},
+]
+
+
+/*
+{ 
+	title: '',
+	author: '',
+	popular: ,
+	group: '',
+	fiction: ,
+	},
+*/
--- js/index.js
+++ js/index.js
@@ -10,3 +10,5 @@ const removeActiveClass = () => {
 
 searchField.addEventListener('focus', addActiveClass);
 searchField.addEventListener('blur', removeActiveClass);
+
+
