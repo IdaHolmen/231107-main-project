ad6e738747a7ebaf987f13221c975ada5af0a52a
IdaHolmen <143997448+IdaHolmen@users.noreply.github.com>
2023-11-16 14:53:14 +0100

    Search field correctly displaying when active

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 const searchField = document.querySelector('.search-field');
 
-console.log(searchField);
+const addActiveClass = () => {
+	searchField.classList.add('search-field--active');
+}
+
+const removeActiveClass = () => {
+	searchField.classList.remove('search-field--active');
+}
+
+searchField.addEventListener('focus', addActiveClass);
+searchField.addEventListener('blur', removeActiveClass);
