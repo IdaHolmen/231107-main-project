1525ebceca0cff60390829dedb8ed2936dfb33b5
IdaHolmen <143997448+IdaHolmen@users.noreply.github.com>
2023-11-18 15:06:20 +0100

    Adding function to search field

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Active color for search field
 const searchField = document.querySelector('.search-field');
 
 const addActiveClass = () => {
@@ -10,5 +11,24 @@ const removeActiveClass = () => {
 
 searchField.addEventListener('focus', addActiveClass);
 searchField.addEventListener('blur', removeActiveClass);
+
+// For searching through the books
+
+const searchBooks = () => {
+	const input = searchField.value.toLowerCase();
+	let bookContainers = document.querySelectorAll('.book__container');
+
+	for (i = 0; i < bookContainers.length; i++) {
+		let bookTitle = document.querySelectorAll('.book__title').innerText.toLowerCase();
+		let bookAuthor = document. querySelectorAll('.book__author').innerText.toLowerCase();
+
+		if (bookTitle.includes(input) || bookAuthor.includes(input)) {
+			bookContainers[i].style.display = '',
+		} else {
+			bookContainers[i].style.display = 'none';
+		}
+	}	
+};
+
 
 
